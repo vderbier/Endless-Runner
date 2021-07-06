d35e01fbba7b4fa3660502efa753d763839eda1d
Bryson Frank <bmfrank@ucsc.edu>
2021-07-06 08:52:50 -0700

    Game Over screen

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -3,8 +3,14 @@ class GameOver extends Phaser.Scene {
         super("GameOver");
     }
 
+    preload() {
+        this.load.image('gameOver', './Assets/GameOver-01.png');
+      }
+      
+
     create() {
-        this.scoreConfig = {
+        this.add.image(0, 0, 'gameOver').setOrigin(0, 0);
+        /*this.scoreConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
             backgroundColor: '#Fb3141',
@@ -17,6 +23,7 @@ class GameOver extends Phaser.Scene {
             fixedWidth: 170
         }
         this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', this.scoreConfig).setOrigin(0.5);
+        */
         this.GameOver = true;
 
         this.keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
