29f9131e05c45f2b9f442a9dc5dcc1d5b18c2675
Bryson Frank <bmfrank@ucsc.edu>
2021-07-06 05:34:09 -0700

    Menu Scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,10 +1,21 @@
-
 class Menu extends Phaser.Scene {
-    constructor() {
-      super("menuScene");
+  constructor() {
+    super("menuScene");
+  }
+  
+  preload() {
+    this.load.image('title', './Assets/TitleScene-01.png');
+  }
+  
+  create() {
+    this.add.image(0, 0, 'title').setOrigin(0, 0);
+      
+    this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  }
+  
+  update(){
+    if (Phaser.Input.Keyboard.JustDown(this.upKey)){
+      this.scene.start('playScene');
     }
-    
-    create() {
-      this.scene.start("playScene");
-    }
+  }
 }
